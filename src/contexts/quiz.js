bfe145b6eb7f0d255289280d838646f4d14c84d1
jeewoonk <isusie.k@gmail.com>
2024-01-22 15:17:33 -0800

    as of lec4

--- src/contexts/quiz.js
+++ src/contexts/quiz.js
@@ -1,28 +1,47 @@
 import { createContext, useReducer } from "react";
 import questions from "../data";
+import { shuffleAnswers } from "../helper";
 
 const initialState = {
     currentQuestionIndex: 0,
     questions,
     showResults: false,
+    answers: shuffleAnswers(questions[0]),
+    currentAnswer: '',
+    correctAnswersCount: 0,
 };
 
 const reducer = (state, action) => {
-    if (action.type === 'NEXT_QUESTION') {
-        const showResults = state.currentQuestionIndex === state.questions.length - 1;
-        const currentQuestionIndex = showResults ? state.currentQuestionIndex : state.currentQuestionIndex + 1;
-        return {
-            ...state,
-            currentQuestionIndex,
-            showResults,
-        };
-    }
+    switch (action.type) {
+        case 'NEXT_QUESTION': {
+            const showResults = state.currentQuestionIndex === state.questions.length - 1;
+            const currentQuestionIndex = showResults ? state.currentQuestionIndex : state.currentQuestionIndex + 1;
+            const answers = showResults ? [] : shuffleAnswers(state.questions[currentQuestionIndex]);
+            return {
+                ...state,
+                currentQuestionIndex,
+                showResults,
+                answers,
+                currentAnswer: "",
+            };
+        }
 
-    if (action.type === 'RESTART') {
-        return initialState;
-    }
-    
-    return state;
+        case 'RESTART': {
+            return initialState;
+        }
+        
+        case 'SELECT_ANSWER': {
+            const correctAnswersCount = action.payload === state.questions[state.currentQuestionIndex].correctAnswer ? state.correctAnswersCount + 1: state.correctAnswersCount
+            return {
+                ...state,
+                currentAnswer: action.payload,
+            };
+        }
+
+        default: {
+            return state;
+        }
+    };
 };
 
 
@@ -30,6 +49,5 @@ export const QuizContext = createContext();
 
 export const QuizProvider = ({children}) => {
     const value = useReducer(reducer, initialState);
-    console.log("state", value);
     return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>
 }
